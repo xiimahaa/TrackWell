f2d6e7346aa0812b8075aee08267ee288c561fad
Maha <165519657+xiimahaa@users.noreply.github.com>
2025-07-11 00:16:24 +0300

    Add files via upload

---
+++ Main/TrackWell.java
@@ -0,0 +1,16 @@
+
+package com.mycompany.trackwell;
+
+
+import GUI.*;
+
+
+public class TrackWell {
+
+    public static void main(String[] args) {
+
+        WelcomePage welcomePage = new WelcomePage();
+        welcomePage.setVisible(true);
+
+    }
+}
